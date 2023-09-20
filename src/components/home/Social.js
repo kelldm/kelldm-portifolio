63d9a896c398ece44ec92358c6675bfa77ecad19
Raquel Lins <raquellinsdev@gmail.com>
2023-09-20 08:51:21 -0300

    fix

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -5,9 +5,9 @@ import { BsLinkedin, BsGithub, BsInstagram } from "react-icons/bs";
 function Social (){
     return(
         <div className="social-icons">
-       <a href='https://www.linkedin.com/in/raquelldm/'  target='_blank'> <BsLinkedin size={50} className="icon" /></a>
-       <a href='https://github.com/kelldm'  target='_blank'><BsGithub size={50} className="icon" /> </a> 
-       <a href='https://www.instagram.com/kelldm_/' target='_blank'> <BsInstagram size={50} className="icon" /></a>
+       <a href='https://www.linkedin.com/in/raquelldm/'  target='_blank'> <BsLinkedin size={50} color='#000' className="icon" /></a>
+       <a href='https://github.com/kelldm'  target='_blank'><BsGithub size={50} color='#000' className="icon" /> </a> 
+       <a href='https://www.instagram.com/kelldm_/' target='_blank'> <BsInstagram size={50} color='#000' className="icon" /></a>
       </div>
   
     );
